e78943c5db9593686ed5ce8429a5bae73340482e
BINHHOANG\Binh <slug_93@yahoo.com>
2017-06-23 21:31:42 +0700

    new item w title/price BUT no description.. AND imge

--- routes/views/category.js
+++ routes/views/category.js
@@ -11,6 +11,16 @@ exports = module.exports = function(req, res) {
 	// item in the header navigation.
 	locals.section = 'home';
 	
+
+	view.on('init', function(next){
+		var q = keystone.list('Product').model.find();
+		q.exec(function(err, res) {
+			console.log('data', res.length);
+			locals.data = res;
+			next();
+		})
+	});
+	
 	// Render the view
 	view.render('category');
 	
